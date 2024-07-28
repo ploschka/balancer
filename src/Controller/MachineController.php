95b15d822aba05165a0817c03fa4455749d09e70
Ploshka <folga12381@gmail.com>
2024-07-28 21:19:03 +1000

    Basic adding implemented

--- src/Controller/MachineController.php
+++ src/Controller/MachineController.php
@@ -2,19 +2,24 @@
 
 namespace App\Controller;
 
+use App\Entity\Machine;
+use App\Service\LoadBalancer;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Attribute\Route;
 
 #[Route('/machine')]
 class MachineController extends AbstractController
 {
     #[Route('/add', name: 'add_machine')]
-    public function add(): JsonResponse
+    public function add(Request $r, LoadBalancer $lb): JsonResponse
     {
+        $m = new Machine();
+        $j = json_decode($r->getContent(), true);
+        $m->setMemory($j['memory'])->setCpus($j['cpus']);
+        $lb->findProcessForMachine($m);
         return $this->json([
-            'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/MachineController.php',
         ]);
     }
 
--- src/Controller/ProcessController.php
+++ src/Controller/ProcessController.php
@@ -2,24 +2,29 @@
 
 namespace App\Controller;
 
+use App\Entity\Process;
+use App\Service\LoadBalancer;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Attribute\Route;
 
 #[Route('/process')]
 class ProcessController extends AbstractController
 {
     #[Route('/add', name: 'add_process')]
-    public function add(): JsonResponse
+    public function add(Request $r, LoadBalancer $lb): JsonResponse
     {
+        $p = new Process();
+        $j = json_decode($r->getContent(), true);
+        $p->setMemory($j['memory'])->setCpus($j['cpus']);
+        $lb->findMachineForProcess($p);
         return $this->json([
-            'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/ProcessController.php',
         ]);
     }
 
     #[Route('/remove', name: 'remove_process')]
-    public function remove(): JsonResponse
+    public function remove(Request $r, LoadBalancer $lb): JsonResponse
     {
         return $this->json([
             'message' => 'Welcome to your new controller!',
